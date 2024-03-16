2581f60898dd7f9ec0aaf1f004ef7b85413ef45b
Junaid <risklife12@email.com>
2024-03-16 17:46:31 +0530

    change 1

--- client/src/store/accessTokenSlice.js
+++ client/src/store/accessTokenSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"; 
 
 const initialState = {
    token: JSON.parse(localStorage.getItem("Access Token")) || ""
